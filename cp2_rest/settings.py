6d3c4f77c58af7ec055484e368cfd7c6cd10279b
rkdxofkd <sk9324840@naver.com>
2023-01-18 23:46:20 +0900

    test

--- cp2_rest/settings.py
+++ cp2_rest/settings.py
@@ -49,7 +49,7 @@ DATABASES = {
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [env('DB_HOST')]
 
 
 # Application definition
