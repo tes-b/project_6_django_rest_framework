2d398d60ecd77f716bb1890da00d84c40894b3d6
tes-b <tsyi0313@gmail.com>
2023-01-27 11:50:05 +0900

    fix: bug fix

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
 
-from accounts.views import user_list, user_create, signup
+from accounts.views import signup
 
 
 app_name = 'accounts'
@@ -12,7 +12,7 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(), name = 'logout'),
     path('signup/', views.signup, name = 'signup'),
 
-    # 테스트용.. 추후 삭제 예정
+    # 테스트용.. 추후 삭제 예정 by 태섭
     # path('api/user/', user_list.as_view()),
     # path('api/signup/', user_create.as_view())
 
